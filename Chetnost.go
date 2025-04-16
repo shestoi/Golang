8d0df715bf6d4f56f4daa6d8755f5bc4ad1ebc89
Hello World <porky_grant_3n@icloud.com>
2025-04-16 19:29:54 +0300

    Chetnost

---
+++ Chetnost.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	fmt.Println("Введите число: ")
+	fmt.Scan(&a)
+	if a%2 == 0 {
+		fmt.Println("Число четное")
+	} else {
+		fmt.Println("Число нечетное")
+	}
+}
